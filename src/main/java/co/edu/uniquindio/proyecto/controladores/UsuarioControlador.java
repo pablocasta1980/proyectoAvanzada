65b002326d2b4ad6db129b0620a1eab02c7e56b6
pablocasta1980 <pacastanog@uqvirtual.edu.co>
2025-02-23 18:59:15 -0500

    commit punto 8 guia

--- src/main/java/co/edu/uniquindio/proyecto/controladores/UsuarioControlador.java
+++ src/main/java/co/edu/uniquindio/proyecto/controladores/UsuarioControlador.java
@@ -1,15 +1,16 @@
 package co.edu.uniquindio.proyecto.controladores;
 
+import co.edu.uniquindio.proyecto.servicios.UsuarioServicio;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-private UsuarioServicio usuarioServicio;
+
 
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioControlador {
 
-   
+    private UsuarioServicio usuarioServicio;
 
 }
 
